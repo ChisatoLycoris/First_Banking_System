60affaa44260bc5b0505b7e1606fcb4cc7286e95
ChisatoLycoris <112646171+ChisatoLycoris@users.noreply.github.com>
2022-09-08 14:58:41 +0800

    Add files via upload

---
+++ src/bankSystemProject/FirstBank.java
@@ -0,0 +1,15 @@
+package bankSystemProject;
+
+public class FirstBank {
+
+	public static void main(String[] args) {
+
+
+		
+		Account cola = new Account("cola", "A00002");
+		cola.showMenu();
+		
+
+	}
+
+}
